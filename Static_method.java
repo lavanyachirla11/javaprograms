e284ae44c956135a8285a5e3340eb27a15c93f52
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-08-27 21:24:02 +0530

    Add files via upload

---
+++ Static_method.java
@@ -0,0 +1,19 @@
+package pack1;
+class Test
+{
+	static int x =20;
+	static void show()
+	{
+		System.out.println("Static value = "+ x);
+	}
+}
+
+public class Static_method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test.show();
+
+	}
+
+}
---
+++ Static_variable.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class Static_variable {
+        static int x=10;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Static_variable t1 = new Static_variable();
+		   t1.x=25;
+		   System.out.println(t1.x);  
+		   Static_variable t2 = new Static_variable();
+		   System.out.println(t2.x);  
+
+	}
+
+}
