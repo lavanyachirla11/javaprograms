5921bceeecd52ff9ebd038d2c7cc586a2b685c30
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-09-28 17:35:22 +0530

    Add files via upload

---
+++ Format15.java
@@ -0,0 +1,21 @@
+package formats;
+import java.util.*;
+public class Format15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value : ");
+		int n = sc.nextInt();
+		for(int i =n; i>=1; i--) 
+		{
+			for(int j=1; j<=i; j++) 
+			{
+				System.out.print(j); 
+			}
+			System.out.println();
+		}
+
+	}
+
+}
