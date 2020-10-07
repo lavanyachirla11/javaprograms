d3ba2bb5233b3228d7cdb408c85b188e77bbdf2d
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-10-07 17:36:25 +0530

    Add files via upload

---
+++ Format40.java
@@ -0,0 +1,36 @@
+package formats;
+import java.util.Scanner;
+public class Format40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value : ");
+		int n = sc.nextInt();
+		int x=1;
+		for(int i=1; i<=n; i++) 
+		{
+			for(int j =n-1; j>= i; j--)
+				System.out.print(" "); 
+			
+			for(int k=x; k>= 1; k-- ) 
+			{
+				if(i==k)
+					System.out.print(Math.abs(k)); 
+				else
+					System.out.print(Math.abs(k-i)); 
+					
+			} 
+			
+			System.out.println();
+			x= x+2;
+		}
+
+
+
+	}
+
+}
+/*Sample Output:
+
+*/
