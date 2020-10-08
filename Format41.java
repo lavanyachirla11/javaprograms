ce4535eafbdc2a74db4d7f9909e433668e2ad5c6
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-10-08 17:53:03 +0530

    Add files via upload

---
+++ Format41.java
@@ -0,0 +1,30 @@
+package formats;
+import java.util.Scanner;
+public class Format41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value : ");
+		int n = sc.nextInt();
+		int space=n-1;
+		int star=n*2-1;
+		for(int i=n; i>=1; i--) 
+		{
+			for(int j =space; j>= i; j--)
+				System.out.print(" "); 
+			
+			for(int k=1; k<= star; k++ ) 
+			{
+				
+					System.out.print("*"); 
+					
+			} 
+			
+			System.out.println();
+			star= star-2;
+		}
+
+	}
+
+}
