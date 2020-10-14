bf822fced45e66a378d4ed9843bda9e6fc4906bd
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-10-14 17:57:59 +0530

    Add files via upload

---
+++ Format66.java
@@ -0,0 +1,32 @@
+package formats;
+
+import java.util.Scanner;
+
+public class Format66 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter n value : ");
+		int n = sc.nextInt();
+		for(int i=n; i>=1; i--)
+		{
+			for(int j=n-1; j>=i; j--)
+				System.out.print(" ");
+			for(int k=1; k<=i; k++)
+				System.out.print((char)(k+64)+" ");
+			System.out.println();
+		}
+
+
+	}
+
+}
+/* Sample Output:
+Enter n value : 5
+A B C D E 
+ A B C D 
+  A B C 
+   A B 
+    A 
+*/
