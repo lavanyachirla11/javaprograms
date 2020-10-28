64b2f8b0990fd2daae7c39eaedfe71f3e58eb1d0
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-10-28 17:57:53 +0530

    Add files via upload

---
+++ Format90.java
@@ -0,0 +1,37 @@
+package formats;
+import java.util.Scanner;
+public class Format90 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter n value : ");
+		int n = sc.nextInt();
+		int x = n;
+		int y = n;
+		
+		for(int i=1; i<= n; i++) 
+		{
+			for(int j=1; j < n*2; j++)
+			{
+				if(j > x && j <y) 
+					System.out.print(" "); 
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+			x= x-1; 
+			y= y+1; 
+		}
+
+	}
+
+}
+/*Sample Output:
+Enter n value : 6
+***********
+***** *****
+****   ****
+***     ***
+**       **
+*         *
+*/
