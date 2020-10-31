e277a8628410da97cb93a9fb7dc5446719d194aa
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-10-31 18:52:55 +0530

    Add files via upload

---
+++ Drawing_String.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+
+/*<applet code = "Drawing_String.class" width=200 height =300>
+ </applet>*/
+public class Drawing_String extends Applet{
+
+	public void paint(Graphics g)
+	{
+		g.drawString("Lavanya",100, 200);
+
+	}
+
+}
