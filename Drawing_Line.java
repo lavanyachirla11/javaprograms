a2026034e4a3f29fd638b5be9b4c46bde1bdaddc
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-10-31 21:03:56 +0530

    Add files via upload

---
+++ Drawing_Line.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+/*<applet code = "Drawing_Line.class" width=200 height =300>
+ </applet>*/
+public class Drawing_Line extends Applet{
+
+	public void paint(Graphics g)
+	{
+		g.drawLine(100,100,200, 200);
+
+	}
+
+}
